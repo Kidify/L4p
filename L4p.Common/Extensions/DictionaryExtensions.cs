fccf5bd8d2b3d84030d35efeb594f857af7aa9fb
Reuven Bass <rbass@cpp2mtl.com>
2016-04-03 09:26:32 +0300

    GetOrCreate as dictionary extension method

---
+++ L4p.Common/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace L4p.Common.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetOrCreate<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
+            where TValue : new()
+        {
+            TValue value;
+
+            if (!dict.TryGetValue(key, out value))
+            {
+                value = new TValue();
+                dict.Add(key, value);
+            }
+
+            return value;
+        }
+    }
+}
